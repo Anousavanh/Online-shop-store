0c7fc3afcc97fd61dd2b13a885f2599d3b741bc7
Anousavanh <anousavanhaddy@gmail.com>
2024-07-20 16:03:20 +0700

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
       <Image src="/banner1.png" alt="banner" width={2000} height={1000} className="w-screen" />
       <Collections />
       <ProductList />
+      dsads
       <Footer />
     </>
   );
