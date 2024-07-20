d77cd177b1f4ed0d2acfeac39bfc9bd16bd009d0
Anousavanh <anousavanhaddy@gmail.com>
2024-07-20 17:10:50 +0700

    Update i18n.ts

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,21 +1,21 @@
-// lib/i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-http-backend';
 
 i18n
-  .use(Backend)
+  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    supportedLngs: ['en', 'lo'],
     fallbackLng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
+    detection: {
+      order: ['queryString', 'cookie'],
+      caches: ['cookie']
     },
     backend: {
-      loadPath: '/app/locales/{{lng}}/translation.json',
+      loadPath: '/locales/{{lng}}/translation.json',
     },
   });
 
