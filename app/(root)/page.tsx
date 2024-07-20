92d3366a6f8ed490e93c1252a9900fc63bf3110e
Anousavanh <anousavanhaddy@gmail.com>
2024-07-20 15:53:40 +0700

    new

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,16 @@
-import Collections from "@/components/Collections";
-import Footer from "@/components/Footer";
-import ProductList from "@/components/ProductList";
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-
-import Image from "next/image";
+import Collections from '@/components/Collections';
+import Footer from '@/components/Footer';
+import ProductList from '@/components/ProductList';
+import Image from 'next/image';
 
 export default function Home() {
   const { t } = useTranslation();
+
   return (
     <>
-      <Image src="banner1.png" alt="banner" width={2000} height={1000} className="w-screen" />
+      <Image src="/banner1.png" alt="banner" width={2000} height={1000} className="w-screen" />
       <h1>{t('welcome_message')}</h1>
       <Collections />
       <ProductList />
@@ -18,5 +19,4 @@ export default function Home() {
   );
 }
 
-export const dynamic = "force-dynamic";
-
+export const dynamic = 'force-dynamic';
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '@/lib/i18n';
-import '../globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
