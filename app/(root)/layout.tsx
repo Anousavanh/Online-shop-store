d9f0441357454d7ec610d641f0781e96cd96c2e6
Anousavanh <anousavanhaddy@gmail.com>
2024-07-20 16:16:19 +0700

    new

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,8 +5,6 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "../globals.css";
 import Navbar from "@/components/Navbar";
 import ToasterProvider from "@/lib/providers/ToasterProvider";
-import i18n from '@/lib/i18n';
-import { I18nextProvider } from 'react-i18next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,13 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <I18nextProvider i18n={i18n}>
         <ClerkProvider>
           <ToasterProvider />
           <Navbar />
           {children}
         </ClerkProvider>
-        </I18nextProvider>
       </body>
     </html>
   );
