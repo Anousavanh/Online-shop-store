d63d07c506bc57680ca18b6a4ffd8b7c3902fb0c
Anousavanh <anousavanhaddy@gmail.com>
2024-07-25 10:39:54 +0700

    Update Collections.tsx

--- components/Collections.tsx
+++ components/Collections.tsx
@@ -8,7 +8,8 @@ const Collections = async () => {
   const { t } = await useTranslation();
   return (
     <div className="flex flex-col items-center gap-10 py-8 px-5">
-      <p className="text-heading1-bold">{t('Collection')}</p>
+      <p className="text-heading1-bold">Test</p>
+      <h1>{t("Collection")}</h1>
       {!collections || collections.length === 0 ? (
         <p className="text-body-bold">No collections found</p>
       ) : (
